07f7735be517c5dcec7e12ddc278a7580be81109
“DoveCrying” <zenolino@gmail.com>
2022-11-21 21:26:42 +0000

    Added dropdown menu function for admin to contact user.

--- list_eval.php
+++ list_eval.php
@@ -26,7 +26,6 @@ if ($stmt = $con->prepare('SELECT admin FROM accounts WHERE id = ?'))
 	$stmt->bind_result($admin);
 	$stmt->fetch();
 	$stmt->close();
-	echo $admin;
 	if ($admin == 0)
 	{
 	}
@@ -36,6 +35,8 @@ if ($stmt = $con->prepare('SELECT admin FROM accounts WHERE id = ?'))
 		$result = $con->query($query);
 	}
 }
+
+
 ?>
 
 
@@ -47,19 +48,43 @@ if ($stmt = $con->prepare('SELECT admin FROM accounts WHERE id = ?'))
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 		<style>
+select{
+    text-align-last:right;
+    padding-right: 29px;
+    direction: rtl;
+	width:120px;   
+
+}
 			td {
     word-break: break-all;
     word-break: break-word;
 }
-		form input[type="submit"] {
-    width: 100%;
-    background-color: #ff6961;
-   border: 0;
-    cursor: pointer;
-    font-weight: bold;
-    color: #ffffff;
-    transition: background-color 0.2s;
-}
+		#list{
+			font-size: 10px;
+			text-align: center;
+			font-weight: bold;
+			appearance:listbox;
+			width: 100%;
+			background-color: #90EE90;
+			border: 1;
+			border-color: black;
+	}
+		#remove{
+			width: 100%;
+			background-color: #ff6961;
+			border: 0;
+			cursor: pointer;
+			font-weight: bold;
+			transition: background-color 0.2s;
+	}
+		#contact{
+			width: 100%;
+			background-color: #FFD580;
+			border: 0;
+			cursor: pointer;
+			font-weight: bold;
+			transition: background-color 0.2s;
+	}
 		table {
 		border-collapse: collapse;
 		}
@@ -98,13 +123,43 @@ if ($stmt = $con->prepare('SELECT admin FROM accounts WHERE id = ?'))
 					<th>Header</th>
 					<th>Comment</th>
 					<th>Image</th>
+					<th>Contact</th>
+					<th>Resolved</th>
+					<th>Remove</th>
   				</tr>
-                <?php foreach ($result as $row): array_map('htmlentities', $row); ?>
+                <?php foreach ($result as $key => $row): array_map('htmlentities', $row); ?>
                 <tr>
                 <td><?php echo implode('</td><td>', $row); ?></td>
-				<td> 
-				<form action="index.html">
-			<input type="submit" value="Remove" />
+				<td> <form action="index.html">
+			<select id="list" value="Contact">
+				<?php
+					// Get all the categories from category table
+					$sql = "SELECT * FROM `accounts` where id=2";
+					$all_categories = mysqli_query($con,$sql);
+					// use a while loop to fetch data
+					// from the $all_categories variable
+					// and individually display as an option
+					$category = mysqli_fetch_array($all_categories,MYSQLI_ASSOC);
+				?>
+                <option value="<?php echo $category["id"];
+                    // The value we usually set is the primary key
+                ?>">
+                    <?php echo $category["email"];
+                        // To show the category name to the user
+                    ?>
+                </option>
+                <option value="<?php echo $category["id"];
+                    // The value we usually set is the primary key
+                ?>">
+                    <?php echo $category["username"];
+                        // To show the category name to the user
+                    ?>
+                </option>
+			</select>
+			<td> 
+			<input type="submit" value="Contact" id="contact"/>
+				<td>
+			<input type="submit" value="Remove" id="remove"/>
 				</form>
 				</td>
                 </tr>
