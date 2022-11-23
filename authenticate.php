b372f6861d47a17c671586725f37ccc9b4136145
“DoveCrying” <zenolino@gmail.com>
2022-11-23 16:05:48 +0000

    Added max tries to login.

--- authenticate.php
+++ authenticate.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 $empty = FALSE;
+
+++$_SESSION['counter'];
+
 if (isset($_POST) & !empty($_POST))
 {
 	if(empty($_POST['username'])) { $_SESSION['usernameError'] = "Insert Username";$empty = TRUE; }
@@ -27,6 +30,32 @@ if ( mysqli_connect_errno() ) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 
+if ($_SESSION['counter'] > 3)
+{
+	$_SESSION['counter'] = 0;
+	$ip = $_SERVER["REMOTE_ADDR"];
+	mysqli_query($connection, "INSERT INTO `ip` (`address` ,`timestamp`)VALUES ('$ip',CURRENT_TIMESTAMP)");
+	$result = mysqli_query($connection, "SELECT COUNT(*) FROM `ip` WHERE `address` LIKE '$ip' AND `timestamp` > (now() - interval 10 minute)");
+	$count = mysqli_fetch_array($result, MYSQLI_NUM);
+	$_SESSION["error"] = "Please wait 10 minutes before trying again!";	
+	header('Location: login.php');
+	exit();
+}
+
+if ($stmt = $con->prepare('SELECT timestamp FROM ip WHERE address = ?'))
+{
+	$stmt->bind_param('s', $_SERVER["REMOTE_ADDR"]);
+	$stmt->execute();
+	// Store the result so we can check if the account exists in the database.
+	$stmt->bind_result($time);
+	$stmt->fetch();
+	if (!empty($time))
+	{
+		$_SESSION["error"] = "Please wait 10 minutes before trying again!";	
+		header('Location: login.php');
+		exit();
+	}
+}
 // Now we check if the data from the login form was submitted, isset() will check if the data exists.
 if ( !isset($_POST['username'], $_POST['password']) ) {
 	// Could not get the data that should have been sent.
