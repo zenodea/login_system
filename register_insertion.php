6864cd53d149cc811253c54d95a88d461e08dd18
“DoveCrying” <zenolino@gmail.com>
2022-11-24 20:13:04 +0000

    Improved checking information. Added Security Question checking.

--- register_insertion.php
+++ register_insertion.php
@@ -3,6 +3,8 @@ error_reporting(E_ALL);
 ini_set('display_errors',1);
 session_start();
 
+$error = array();
+
 // Change this to your connection info.
 $DATABASE_HOST = '127.0.0.1';
 $DATABASE_USER = 'root';
@@ -15,56 +17,28 @@ if ( mysqli_connect_errno() ) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
     echo "yikes";
 }
-$NEW_USERNAME = $_POST['username'];
-$NEW_EMAIL = $_POST['email'];
-$NEW_PASSWORD =  $_POST['password'];
-$NEW_PHONE = $_POST['phone'];
 
-$error = array();
+$NEW_USERNAME 	= $_SESSION['username'];
+$NEW_EMAIL  	= $_SESSION['email'];
+$NEW_PASSWORD 	=  $_SESSION['password'];
+$NEW_PHONE 		= $_SESSION['phone'];
 
-// Validate password strength
-$uppercase = preg_match('@[A-Z]@', $NEW_PASSWORD);
-$lowercase = preg_match('@[a-z]@', $NEW_PASSWORD);
-$number    = preg_match('@[0-9]@', $NEW_PASSWORD);
-$specialChars = preg_match('@[^\w]@', $NEW_PASSWORD);
+$question_one = array($_POST['first_question'] => $_POST['first_answer']);
+$question_two = array($_POST['second_question'] => $_POST['second_answer']);
+$question_three = array($_POST['third_question'] => $_POST['third_answer']);
 
-if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($NEW_PASSWORD) < 8) 
+if (array_key_exists(key($question_one), $question_two) 
+	| array_key_exists(key($question_one), $question_three)
+	| array_key_exists(key($question_two), $question_one)
+	| array_key_exists(key($question_two), $question_three)
+	| array_key_exists(key($question_three), $question_one)
+	| array_key_exists(key($question_three), $question_two))
 {
-	if (!$uppercase)
-	{
-		array_push($error,"Password has to contain at least one UpperCase Letter!");
-	}
-	if (!$lowercase)
-	{
-		array_push($error,"Password has to contain at least one LowerCase Letter!");
-	}
-	if (!$number)
-	{
-		array_push($error,"Password has to contain at least one Number Letter!");
-	}
-	if (!$specialChars)
-	{
-		array_push($error,"Password has to contain at least one Special Character!");
-	}
-	if (strlen($NEW_PASSWORD) < 8)
-	{
-		array_push($error,"Password has to longer than 8 characters!");
-	}
+	array_push($error,"Please choose three distinct security questions!");
 	$_SESSION['error'] = $error;
-    header('Location: register.php');
+	header('Location: security_questions.php');
 	exit();
 }
-
-
-#Check Email is correct
-if (!filter_var($NEW_EMAIL, FILTER_VALIDATE_EMAIL)) 
-{
-		array_push($error,"Invalid email!");
-		$_SESSION['error'] = $error;
-		header('Location: register.php');
-		exit();
-}
-
 
 if ($stmt = $con->prepare('SELECT id FROM accounts WHERE username = ?')) 
 {
@@ -83,26 +57,39 @@ if ($stmt = $con->prepare('SELECT id FROM accounts WHERE username = ?'))
 		// Username doesnt exists, insert new account
 		if ($stmt = $con->prepare('INSERT INTO accounts (username, pass, email, phone_no, admin, activation_code) VALUES (?, ?, ?, ?, ?, ?)')) 
 		{
-			// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
-			$admin = 0;
-			$password = password_hash($NEW_PASSWORD, PASSWORD_DEFAULT);
-			$uniqid = uniqid();
-			$stmt->bind_param('ssssis', $NEW_USERNAME, $password, $NEW_EMAIL, $NEW_PHONE, $admin, $uniqid);
-			$stmt->execute();
-			$from    = 'noreply@yourdomain.com';
-			$subject = 'Account Activation Required';
-			$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
-			// Update the activation variable below
-			$activate_link = 'http://yourdomain.com/phplogin/activate.php?email=' . $NEW_EMAIL . '&code=' . $uniqid;
-			$message = '<p>Please click the following link to activate your account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
-			mail($NEW_EMAIL, $subject, $message, $headers);
-			$success = array();
-			array_push($success,'Account Succesfully Created!');
-			array_push($success,'An email has been sent with an activation link.');
-			array_push($success,'please Activate your Account!');
-			$_SESSION['success'] = $success;
-			header('Location: register.php');
-			exit();
+				// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+				$admin = 0;
+				$password = password_hash($NEW_PASSWORD, PASSWORD_DEFAULT);
+				$uniqid = uniqid();
+				$stmt->bind_param('ssssis', $NEW_USERNAME, $password, $NEW_EMAIL, $NEW_PHONE, $admin, $uniqid);
+				$stmt->execute();
+			if  ($stmt = $con->prepare('INSERT INTO security_questions VALUES (?, ?, ?, ?, ?, ?, ?)'))
+			{
+				$result = $con->insert_id;
+				// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+				$stmt->bind_param('iisisis', $result, key($question_one), current($question_one), key($question_two), current($question_two),
+								  key($question_three), current($question_three),);
+				$stmt->execute();
+				$from    = 'noreply@yourdomain.com';
+				$subject = 'Account Activation Required';
+				$headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+				// Update the activation variable below
+				$activate_link = 'http://yourdomain.com/phplogin/activate.php?email=' . $NEW_EMAIL . '&code=' . $uniqid;
+				$message = '<p>Please click the following link to activate your account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
+				mail($NEW_EMAIL, $subject, $message, $headers);
+				$success = array();
+				array_push($success,'Account Succesfully Created!');
+				array_push($success,'An email has been sent with an activation link.');
+				array_push($success,'please Activate your Account!');
+				$_SESSION['success'] = $success;
+				header('Location: register.php');
+				exit();
+			}
+			else
+			{
+			// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+			echo 'Could not prepare statement!';
+			}
 		} 
 		else 
 		{
--- security_questions.php
+++ security_questions.php
@@ -33,12 +33,29 @@
   $token = md5(uniqid(rand(), true));
   $_SESSION['csrf_token'] = $token;
   $_SESSION['csrf_token_time'] = time();
-?>
 
+  $questions = array(
+    1 => "What city were you born in?",
+    2 => "What is your oldest sibling’s middle name?",
+    3 => "What was the first concert you attended?",
+    4 => "What was the make and model of your first car?",
+    5 => "In what city or town did your parents meet?",
+);
+?>
 
 <!DOCTYPE html>
 <html>
 	<head>
+		<style>
+		select {
+			display: inline-block;
+			width: 310px;
+			height: 50px;
+			border: 1px solid #dee0e4;
+			margin-top: 20px;
+			margin-bottom: 20px;
+		}
+        </style>
 		<meta charset="utf-8">
 		<title>Register</title>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
@@ -47,7 +64,7 @@
 	</head>
 	<body>
 		<div class="login">
-			<h1>Register</h1>
+			<h1>Security Questions</h1>
 			<?php
 			if (isset($_SESSION["error"]) & !empty($_SESSION["error"])) 
 			{
@@ -66,24 +83,29 @@
 			}
 			$_SESSION['success'] = NULL;
 			?>
-			<form action="checkInformation.php" method="POST" autocomplete="off">
+			<form action="register_insertion.php" method="POST" autocomplete="off">
 			<input type="hidden" name="csrf_token" value="<?php echo $token;?>">
-				<label for="username">
-					<i class="fas fa-user"></i>
-				</label>
-				<input type="text" name="username" placeholder="Username" id="username" required>
-				<label for="email">
-					<i class="fas fa-envelope"></i>
-				</label>
-				<input type="text" name="email" placeholder="email" id="email" required>
-				<label for="phone">
-					<i class="fas fa-phone"></i>
-				</label>
-				<input type="text" name="phone" placeholder="Phone" id="phone" required>
-				<label for="password">
-					<i class="fas fa-lock"></i>
-				</label>
-				<input type="password" name="password" placeholder="Password" id="password" required>
+            <select name="first_question" id="first_question">
+                <option value="">--- select security question ---</option>
+            <?php foreach($questions as $value => $key): ?>
+                <option value=<?= $value; ?>><?= $key; ?></option>
+            <?php endforeach; ?>
+            </select>
+				<input type="text" name="first_answer" placeholder="Answer" id="first_answer" required>
+            <select name="second_question" id="second_question">
+                <option value="">--- Select Security Question ---</option>
+            <?php foreach($questions as $value => $key): ?>
+                <option value=<?= $value; ?>><?= $key; ?></option>
+            <?php endforeach; ?>
+            </select>
+				<input type="text" name="second_answer" placeholder="Answer" id="second_answer" required>
+            <select name="third_question" id="third_question">
+                <option value="">--- Select Security Question ---</option>
+            <?php foreach($questions as $value => $key): ?>
+                <option value=<?= $value; ?>><?= $key; ?></option>
+            <?php endforeach; ?>
+            </select>
+				<input type="text" name="third_answer" placeholder="Answer" id="third_answer" required>
 				<input type="submit" value="Register">
 			</form>
 		<form action="login.php">
