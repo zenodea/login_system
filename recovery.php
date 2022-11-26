c87e8f7152b90a5bd4874522117dc9d8f6f7f6a5
DoveCrying <zenolino@gmail.com>
2022-11-26 12:28:35 +0000

    Began recovery password function

--- recovery.php
+++ recovery.php
@@ -1,66 +1,52 @@
 <?php
-  session_start(); // must be before any output
-  if(isset($_POST) & !empty($_POST))
-  {
-	if(isset($_POST['csrf_token']))
-	{
-		if($_POST['csrf_token'] == $_SESSION['csrf_token'])
-		{
-			echo"token recognized";
-		}
-		else
-		{
-			$errors[] = "Issues With Token";
-		}
-	}
-	$maximum_time = 5;
-	if (isset($_SESSION['csrf_token_time']))
-	{
-		$token_time = $_SESSION['csrf_token_time'];
-		if(($token_time + $maximum_time) >= time())
-		{
-			unset($_SESSION['csrf_token_time']);
-			unset($_SESSION['csrf_token']);
-			$errors[] = 'token expired';
-		}
-		else
-		{
-			echo  "all good";
-		}
-	}
-  }
-  $token = md5(uniqid(rand(), true));
-  $_SESSION['csrf_token'] = $token;
-  $_SESSION['csrf_token_time'] = time();
-?>
+$error = array();
 
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Recovery pssword</title>
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
-		<link href="style.css" rel="stylesheet" type="text/css">
-	</head>
-	<body>
-		<div class="login">
-			<h1>Recovery</h1>
-			<form action="authenticate.php" method="POST" class="signup-form">
-			<input type="hidden" name="csrf_token" value="<?php echo $token;?>">
-                <label for="Username">
-                    <i class="fas fa-user"></i>
-                </label>
-                <input type="text" name="user" placeholder="user" id="user" required>
-				<input type="submit" value="Submit Password Recovery Request">
-			</form>
-		<form action="register.php">
-		<input type="hidden" name="csrf_token" value="<?php echo $token;?>">
-			<input type="submit" value="Register" />
-		</form>
-        <form action="login.php">
-		<input type="hidden" name="csrf_token" value="<?php echo $token;?>">
-			<input type="submit" value="Login" />
-		</form>
-		</div>
-	</body>
-</html>
+// Change this to your connection info.
+$DATABASE_HOST = '127.0.0.1';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'firstexample';
+
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+
+if ( mysqli_connect_errno() ) {
+	// If there is an error with the connection, stop the script and display the error.
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+    echo "yikes";
+}
+
+if ($stmt = $con->prepare('SELECT mail FROM accounts WHERE username = ?'))
+{
+	$stmt->bind_param('s',$_POST['user']);
+	$stmt->execute();
+	$stmt->bind_result($email);
+	$stmt->fetch();
+	$stmt->close();
+	if ($email == 0)
+	{
+        array_push($error,'please Activate your Account!');
+        $_SESSION['error'] = $success;
+        header('Location: register.php');
+        exit();
+	}
+	else
+	{
+		$query = "SELECT id, id_user, header, comment, url, contact FROM evaluations";
+		$result = $con->query($query);
+	}
+    $from    = 'noreply215872@gmail.com';
+    $subject = 'Account Activation Required';
+    $headers = 'From: ' . $from . "\r\n" . 'Reply-To: ' . $from . "\r\n" . 'X-Mailer: PHP/' . phpversion() . "\r\n" . 'MIME-Version: 1.0' . "\r\n" . 'Content-Type: text/html; charset=UTF-8' . "\r\n";
+    // Update the activation variable below
+    $activate_link = 'localhost/ComputerSecurity/activate.php?email=' . $NEW_EMAIL . '&code=' . $uniqid;
+    $message = '<p>Please click the following link to activate your account: <a href="' . $activate_link . '">' . $activate_link . '</a></p>';
+    mail($NEW_EMAIL, $subject, $message, $headers);
+    $success = array();
+    array_push($success,'Account Succesfully Created!');
+    array_push($success,'An email has been sent with an activation link.');
+    array_push($success,'please Activate your Account!');
+    $_SESSION['success'] = $success;
+    header('Location: register.php');
+    exit();
+}
+?>
